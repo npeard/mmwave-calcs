e257ab07b286a654fca9865412d82de4d486dc44
npeard <nolan.peard@gmail.com>
2024-04-22 16:05:34 -0700

    implemented plots for unitary dynamics

--- plotters/plot_rydberg_dynamics.py
+++ plotters/plot_rydberg_dynamics.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import models.rydberg_dynamics as rydnamics
 import matplotlib.colors as colors
 import models.pulse_calcs as pulses
+from tqdm import tqdm
 
 
 def plot_pulse():
@@ -49,7 +50,7 @@ def plot_state_hold_vs_probe_power():
 	Rydberg_final = []
 	pi_pulse_duration = []
 
-	for Pp in probe_powers:
+	for Pp in tqdm(probe_powers):
 		pi_pulse_duration.append(runner.transition.get_PiPulseDuration(Pp=Pp,
 													   Pc=coupling_power))
 		for hold in holds:
@@ -70,6 +71,109 @@ def plot_state_hold_vs_probe_power():
 	ax1.set_ylabel(r'Hold Time (s)')
 	for n in range(1,5,1):
 		ax1.plot(probe_powers, n*pi_pulse_duration, color='cyan')
+
+	plt.tight_layout()
+	plt.show()
+
+
+def plot_state_power_vs_power_fixed_pi():
+	runner = rydnamics.ExciteRydberg()
+
+	coupling_powers = np.linspace(0.1, 10, 20)
+	probe_powers = np.linspace(1e-3, 10e-3, 20)
+	holds = runner.transition.get_PiPulseDuration(Pp=5e-3, Pc=5)
+
+	Ground_list = []
+	Inter_list = []
+	Rydberg_final = []
+	Sweep_list = []
+	pi_pulse_duration = []
+	Rabi_ratio = []
+
+	for Pp in tqdm(probe_powers):
+		for Pc in coupling_powers:
+			Ground, Inter, Rydberg, Sweep, _ = runner.probe_pulse_unitary(duration=0e-9,
+														  delay=5e-9,
+														  hold=holds, probe_peak_power=Pp,
+														  couple_power=Pc)
+			# Ground_list.append(Ground)
+			# Inter_list.append(Inter)
+			Rydberg_final.append(Rydberg[-1])
+			Rabi_ratio.append(
+				runner.func_Omega23_from_Power(Pc) /
+				runner.func_Omega12_from_Power(
+					Pp))
+		# Sweep_list.append(Sweep)
+	Ryd_pop = np.asarray(Rydberg_final)
+	Ryd_pop = np.reshape(Ryd_pop, (len(probe_powers), len(coupling_powers))).T
+
+	Rabi_ratio = np.asarray(Rabi_ratio)
+	Rabi_ratio = np.reshape(Rabi_ratio,
+							(len(probe_powers), len(coupling_powers))).T
+
+	fig, (ax1, ax2) = plt.subplots(nrows=2)
+	s1 = ax1.imshow(Ryd_pop, vmax=np.max(Ryd_pop), aspect='auto',
+					origin="lower",
+					extent=[np.min(probe_powers), np.max(probe_powers),
+							np.min(coupling_powers), np.max(coupling_powers)])
+	cbar = fig.colorbar(s1, ax=ax1)
+	cbar.set_label(r'Rydberg Population')
+	ax1.set_xlabel(r'Peak 456nm Power (W)')
+	ax1.set_ylabel(r'1064nm Power (W)')
+	ax1.scatter(5e-3, 5, color='cyan', marker='x', s=100)
+
+	s2 = ax2.imshow(Rabi_ratio, aspect='auto', origin="lower",
+					extent=[np.min(probe_powers), np.max(probe_powers),
+							np.min(coupling_powers), np.max(coupling_powers)])
+	cbar = fig.colorbar(s2, ax=ax2)
+	cbar.set_label(r'Rabi_2/Rabi_1')
+	ax2.set_xlabel(r'Peak 456nm Power (W)')
+	ax2.set_ylabel(r'1064nm Power (W)')
+
+	plt.tight_layout()
+	plt.show()
+
+
+def plot_state_couple_power_vs_detune(coupling_powers=None, detunings=None,
+									   probe_peak_power=None, duration=None):
+	runner = rydnamics.ExciteRydberg()
+
+	Ground_list = []
+	Inter_list = []
+	Rydberg_final = []
+	Sweep_list = []
+	pi_pulse_duration = []
+	optimal_detuning = []
+
+	for Delta in tqdm(detunings):
+		for Pc in coupling_powers:
+			hold = runner.transition.get_PiPulseDuration(Pp=probe_peak_power,
+														 Pc=Pc)
+			Ground, Inter, Rydberg, Sweep, _ = runner.probe_pulse_unitary(
+				duration=duration,
+														  delay=5e-9, hold=hold,
+														  probe_peak_power=probe_peak_power,
+																		  couple_power=Pc,
+														  Delta=Delta)
+			Rydberg_final.append(Rydberg[-1])
+	Ryd_pop = np.asarray(Rydberg_final)
+	Ryd_pop = np.reshape(Ryd_pop, (len(detunings), len(coupling_powers))).T
+
+	for Pc in coupling_powers:
+		optimal_detuning.append(runner.transition.get_OptimalDetuning(
+			P1=probe_peak_power, P2=Pc))
+	optimal_detuning = np.asarray(optimal_detuning)
+
+	fig, (ax1) = plt.subplots(nrows=1)
+	s1 = ax1.imshow(Ryd_pop, vmax=np.max(Ryd_pop), aspect='auto',
+					origin="lower",
+					extent=[np.min(detunings), np.max(detunings),
+							np.min(coupling_powers), np.max(coupling_powers)])
+	cbar = fig.colorbar(s1, ax=ax1)
+	cbar.set_label(r'Rydberg Population')
+	ax1.set_xlabel(r'Detuning (2pi x GHz)')
+	ax1.set_ylabel(r'1064nm Power (W)')
+	ax1.plot(optimal_detuning, coupling_powers, color='cyan')
 
 	plt.tight_layout()
 	plt.show()
