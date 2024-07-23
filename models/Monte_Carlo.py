8a9e0c132c66f1459e01e04736c817ed91f3c16e
linxin1994 <157413223+linxin1994@users.noreply.github.com>
2024-07-23 16:47:24 -0700

    Add files via upload
    Correct the comment

--- models/Monte_Carlo.py
+++ models/Monte_Carlo.py
@@ -4,8 +4,54 @@ from arc import *
 import scipy.constants as const
 from scipy.integrate import odeint
 
-def Monte_Carlo_iHO_3d(atom, t, omega_H, omega_L, T, N_ensemble, traping_laser, D2_line):
+def Monte_Carlo_iHO_3d(atom, t, omega_H, omega_L, T, N_ensemble, traping_laser, D2_line, do_iHO = False):
+    """
+    Perform a Monte Carlo simulation of a 3D harmonic oscillator with an inhomogeneous
+    potential. The simulation is performed using the odeint function from the scipy.integrate
+    module. The function takes in the following parameters:
     
+    Parameters:
+    ----------
+    atom : object
+        An instance of the Atom class representing the atom being simulated.
+    t : array-like
+        An array of time values at which to evaluate the simulation.
+    omega_H : float
+        The frequency of the horizontal oscillation.
+    omega_L : float
+        The frequency of the vertical oscillation.
+    T : float
+        The temperature of the system.
+    N_ensemble : int
+        The number of ensemble members to generate.
+    traping_laser : float
+        The wavelength of the trapping laser.
+    D2_line : float
+        The wavelength of the D2 line.
+    do_iHO : bool, optional
+        Flag indicating whether to include the inverter harmonics potential (default: False).
+    
+    Returns:
+    -------
+    x_ensemble_t : ndarray
+        An array of shape (N_ensemble, len(t)) containing the x coordinates of the ensemble
+        members at each time step.
+    vx_ensemble_t : ndarray
+        An array of shape (N_ensemble, len(t)) containing the x velocities of the ensemble
+        members at each time step.
+    y_ensemble_t : ndarray
+        An array of shape (N_ensemble, len(t)) containing the y coordinates of the ensemble
+        members at each time step.
+    vy_ensemble_t : ndarray
+        An array of shape (N_ensemble, len(t)) containing the y velocities of the ensemble
+        members at each time step.
+    z_ensemble_t : ndarray
+        An array of shape (N_ensemble, len(t)) containing the z coordinates of the ensemble
+        members at each time step.
+    vz_ensemble_t : ndarray
+        An array of shape (N_ensemble, len(t)) containing the z velocities of the ensemble
+        members at each time step.
+    """
     def wavelength2frequency (wavelength):
         from scipy.constants import c as c_c
         return c_c/(wavelength)
@@ -18,29 +64,21 @@ def Monte_Carlo_iHO_3d(atom, t, omega_H, omega_L, T, N_ensemble, traping_laser, 
         return ax, ay, az
     
     def newton_3d(f, t, omega_H, omega_L):
-        x, vx, y, vy, z, vz = f
+        x, y, z, vx, vy, vz = f
         ax, ay, az = iHO_a_3d(x, y, z, omega_H, omega_L)
-        dfdt = [vx, ax, vy, ay, vz, az]
+        dfdt = [vx, vy, vz, ax, ay, az]
         return dfdt
 
     m = atom.mass
-    delta_x = np.sqrt(const.k*T/(m*omega_H**2))
-    delta_vx = np.sqrt(const.k*T/m)
-    delta_y = np.sqrt(const.k*T/(m*omega_H**2))
-    delta_vy = np.sqrt(const.k*T/m)
-    delta_z = np.sqrt(const.k*T/(m*omega_L**2))
-    delta_vz = np.sqrt(const.k*T/m)
-    x_ensemble = np.random.normal(0, delta_x, N_ensemble)
-    y_ensemble = np.random.normal(0, delta_y, N_ensemble)
-    z_ensemble = np.random.normal(0, delta_z, N_ensemble)
-    vx_ensemble = np.random.normal(0, delta_vx, N_ensemble)
-    vy_ensemble = np.random.normal(0, delta_vy, N_ensemble)
-    vz_ensemble = np.random.normal(0, delta_vz, N_ensemble)
+    delta_r = np.sqrt(const.k*T/(m*np.array([omega_H, omega_H, omega_L])**2))
+    delta_v = np.sqrt(const.k*T/m)*np.ones(3)
+    x_ensemble, y_ensemble, z_ensemble =  (delta_r.T * np.random.normal(0, 1, size = (N_ensemble, 3)) ).T
+    vx_ensemble, vy_ensemble, vz_ensemble= (delta_v.T * np.random.normal(0, 1, size = (N_ensemble, 3))).T
 
 
-    v = wavelength2frequency(traping_laser)
-    v0 = wavelength2frequency(D2_line)
-    alpha = abs((v + v0)*(v-v0)/v**2)
+    f = wavelength2frequency(traping_laser)
+    f0 = wavelength2frequency(D2_line)
+    alpha = abs((f + f0)*(f-f0)/f**2) # see paper https://journals.aps.org/pra/abstract/10.1103/PhysRevA.97.053803
     print(f"alpha = {alpha}")
 
     omega_iHO_H = np.sqrt(alpha)*omega_H
@@ -53,14 +91,16 @@ def Monte_Carlo_iHO_3d(atom, t, omega_H, omega_L, T, N_ensemble, traping_laser, 
     vy_ensemble_t = []
     vz_ensemble_t = []
 
-    for x, vx, y, vy, z, vz in zip(x_ensemble, vx_ensemble, y_ensemble, vy_ensemble, z_ensemble, vz_ensemble):
-        f0 = [x, vx, y, vy, z, vz]
-        sol = odeint(newton_3d, f0, t, args = (omega_iHO_H, omega_iHO_L,))
+    for f0 in zip(x_ensemble, y_ensemble, z_ensemble, vx_ensemble, vy_ensemble, vz_ensemble):
+        if do_iHO:
+            sol = odeint(newton_3d, f0, t, args = (omega_iHO_H, omega_iHO_L,))
+        else:
+            sol = odeint(newton_3d, f0, t, args = (0, 0,))
         x_ensemble_t.append(sol[:, 0])
-        vx_ensemble_t.append(sol[:, 1])
-        y_ensemble_t.append(sol[:, 2])
-        vy_ensemble_t.append(sol[:, 3])
-        z_ensemble_t.append(sol[:, 4])
+        y_ensemble_t.append(sol[:, 1])
+        z_ensemble_t.append(sol[:, 2])
+        vx_ensemble_t.append(sol[:, 3])
+        vy_ensemble_t.append(sol[:, 4])
         vz_ensemble_t.append(sol[:, 5])
 
     x_ensemble_t = np.array(x_ensemble_t)
@@ -73,73 +113,6 @@ def Monte_Carlo_iHO_3d(atom, t, omega_H, omega_L, T, N_ensemble, traping_laser, 
 
     return x_ensemble_t, vx_ensemble_t, y_ensemble_t, vy_ensemble_t, z_ensemble_t, vz_ensemble_t 
 
-def Monte_Carlo_free_3d(atom, t, omega_H, omega_L, T, N_ensemble, traping_laser, D2_line):
-    
-    def wavelength2frequency (wavelength):
-        from scipy.constants import c as c_c
-        return c_c/(wavelength)
-    
-    def iHO_a_3d (x, y, z, omega_H, omega_L):
-        ax = (omega_H**2)*x
-        ay = (omega_H**2)*y
-        g = 9.8 #m/s^2 gravity
-        az = (omega_L**2)*z - g
-        return ax, ay, az
-    
-    def newton_3d(f, t, omega_H, omega_L):
-        x, vx, y, vy, z, vz = f
-        ax, ay, az = iHO_a_3d(x, y, z, omega_H, omega_L)
-        dfdt = [vx, ax, vy, ay, vz, az]
-        return dfdt
-
-    m = atom.mass
-    delta_x = np.sqrt(const.k*T/(m*omega_H**2))
-    delta_vx = np.sqrt(const.k*T/m)
-    delta_y = np.sqrt(const.k*T/(m*omega_H**2))
-    delta_vy = np.sqrt(const.k*T/m)
-    delta_z = np.sqrt(const.k*T/(m*omega_L**2))
-    delta_vz = np.sqrt(const.k*T/m)
-    x_ensemble = np.random.normal(0, delta_x, N_ensemble)
-    y_ensemble = np.random.normal(0, delta_y, N_ensemble)
-    z_ensemble = np.random.normal(0, delta_z, N_ensemble)
-    vx_ensemble = np.random.normal(0, delta_vx, N_ensemble)
-    vy_ensemble = np.random.normal(0, delta_vy, N_ensemble)
-    vz_ensemble = np.random.normal(0, delta_vz, N_ensemble)
-
-
-    v = wavelength2frequency(traping_laser)
-    v0 = wavelength2frequency(D2_line)
-    alpha = abs((v + v0)*(v-v0)/v**2)
-    print(f"alpha = {alpha}")
-
-    omega_iHO_H = np.sqrt(alpha)*omega_H
-    omega_iHO_L = np.sqrt(alpha)*omega_L
-
-    x_ensemble_t = []
-    y_ensemble_t = []
-    z_ensemble_t = []
-    vx_ensemble_t = []
-    vy_ensemble_t = []
-    vz_ensemble_t = []
-
-    for x, vx, y, vy, z, vz in zip(x_ensemble, vx_ensemble, y_ensemble, vy_ensemble, z_ensemble, vz_ensemble):
-        f0 = [x, vx, y, vy, z, vz]
-        sol = odeint(newton_3d, f0, t, args = (0, 0,))
-        x_ensemble_t.append(sol[:, 0])
-        vx_ensemble_t.append(sol[:, 1])
-        y_ensemble_t.append(sol[:, 2])
-        vy_ensemble_t.append(sol[:, 3])
-        z_ensemble_t.append(sol[:, 4])
-        vz_ensemble_t.append(sol[:, 5])
-
-    x_ensemble_t = np.array(x_ensemble_t)
-    y_ensemble_t = np.array(y_ensemble_t)
-    z_ensemble_t = np.array(z_ensemble_t)
-    vx_ensemble_t = np.array(vx_ensemble_t)
-    vy_ensemble_t = np.array(vy_ensemble_t)
-    vz_ensemble_t = np.array(vz_ensemble_t)
-
-    return x_ensemble_t, vx_ensemble_t, y_ensemble_t, vy_ensemble_t, z_ensemble_t, vz_ensemble_t
 
 
 def recature_rate_3d(trap_temp, x_ensemble_t, vx_ensemble_t, y_ensemble, vy_ensemble, z_ensemble, vz_ensemble, t, m, omega_H, omega_L):
