63dc3803c106e344a174c702fe6d98e15b3a704d
npeard <nolan.peard@gmail.com>
2024-07-24 13:29:35 -0700

    refactor old monte carlo code, tested basic fitting

--- models/utility.py
+++ models/utility.py
@@ -1,14 +1,14 @@
 import numpy as np
-from scipy.constants import c as C_c
+from scipy.constants import c as c_c
 from scipy.constants import epsilon_0
 
 
 def wavelength2freq(wavelength):
-    return 299792458 / wavelength
+    return c_c / wavelength
 
 
 def freq2wavelength(frequency):
-    return 299792458 / frequency
+    return c_c / frequency
 
 
 def power2field(laserPower, laserWaist):
@@ -16,5 +16,5 @@ def power2field(laserPower, laserWaist):
     # A is area in cm^2
     # E is field in V/m
     maxIntensity = 2 * laserPower / (np.pi * laserWaist**2)
-    electricField = np.sqrt(2.0 * maxIntensity / (C_c * epsilon_0))
+    electricField = np.sqrt(2.0 * maxIntensity / (c_c * epsilon_0))
     return electricField
