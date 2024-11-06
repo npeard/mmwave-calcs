f6f51fbe2bb97a3beeb41a18aeb0f2ab15aa6f90
npeard <nolan.peard@gmail.com>
2024-11-05 16:00:20 -0800

    docstrings for utility.py

--- models/utility.py
+++ models/utility.py
@@ -4,21 +4,79 @@ from scipy.constants import epsilon_0
 
 
 def wavelength2freq(wavelength):
+    """
+    Converts a wavelength to a frequency in Hz.
+
+    Parameters
+    ----------
+    wavelength : float
+        The wavelength in meters.
+
+    Returns
+    -------
+    frequency : float
+        The frequency in Hz.
+    """
     return c_c / wavelength
 
 
 def wavelength2angularfreq(wavelength):
+    """
+    Converts a wavelength to an angular frequency in rad/s.
+
+    Parameters
+    ----------
+    wavelength : float
+        The wavelength in meters.
+
+    Returns
+    -------
+    angular_frequency : float
+        The angular frequency in rad/s.
+    """
     return wavelength2freq(wavelength) * 2 * np.pi
 
 
 def freq2wavelength(frequency):
+    """
+    Converts a frequency to a wavelength in meters.
+
+    Parameters
+    ----------
+    frequency : float
+        The frequency in Hz.
+
+    Returns
+    -------
+    wavelength : float
+        The wavelength in meters.
+    """
     return c_c / frequency
 
 
 def power2field(laserPower, laserWaist):
-    # P is power in W
-    # A is area in cm^2
-    # E is field in V/m
+    """
+    Converts a laser power and waist to an electric field in V/m.
+
+    Parameters
+    ----------
+    laserPower : float
+        The power of the laser, in W.
+    laserWaist : float
+        The waist of the laser, in meters.
+
+    Returns
+    -------
+    electricField : float
+        The electric field of the laser, in V/m.
+
+    Notes
+    -----
+    The formula used to calculate the electric field is
+    E = sqrt(2 * I / (c * epsilon_0)),
+    where I is the intensity in W/m^2, c is the speed of light in m/s,
+    and epsilon_0 is the vacuum permittivity in F/m.
+    """
     maxIntensity = 2 * laserPower / (np.pi * laserWaist**2)
     electricField = np.sqrt(2.0 * maxIntensity / (c_c * epsilon_0))
     return electricField
